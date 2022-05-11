bbbd0ee33ab4ade91abd48f253e1446566fa0aed
Yusong Shi <jackshi.sys@gmail.com>
2022-05-10 21:24:20 -0400

    Shop section 1 finished

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -12,25 +12,172 @@ function Shop() {
           <div className="shop-wide-main">
             <div className="shop-wide-media"></div>
             <div className="shop-wide-text">
-              <h3></h3>
-              <p></p>
+              <h3>Editor's choice: Bikes To Buy 👉🏻 </h3>
+              <h6>Bike Above: Specialized Allez custom build</h6>
+              <p>
+                The best bikes made our live easier, kept us exercised, taught
+                us something new, and helped make things more entertaining. Here
+                are some bikes that our editor really likes. Thank you for
+                reading and thanks for making these extraordinary bikes.
+              </p>
             </div>
           </div>
         </div>
 
         <div className="shop-thin">
           <div className="shop-thin-main">
-            <div className="shop-thin-media"></div>
-            <div className="shop-thin-text">
-              <h3></h3>
-              <p></p>
-            </div>
+            <a
+              href="https://www.specialized.com/us/en/s-works-aethos---dura-ace-di2/p/199439?gclid=Cj0KCQjwmuiTBhDoARIsAPiv6L9olK-cDGSxHzDRdWj3aI6eUz94c3RL4GzVwuQGhW_LyGTyiEaPl6EaAjDJEALw_wcB&color=321703-199439"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="shop-thin-media"></div>
+              <div className="shop-thin-text">
+                <h3>S-Works Aethos - Dura-Ace Di2</h3>
+                <h5>$14,000</h5>
+                <p>
+                  Don’t get us wrong, we’re all for racing. Grand Tour top
+                  steps, Classic’s Monuments—they’re in our DNA. But sometimes
+                  riding simply to ride, riding for the love of riding, is just
+                  what we need. And this is what the Aethos lives for. Sure,
+                  it’s given nothing up to performance, and happens to be the
+                  lightest frame out there, but with no compromise to style…
+                </p>
+              </div>
+            </a>
           </div>
 
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="shop-thin-main">
+            <a
+              href="https://www.giant-bicycles.com/us/tcr-advanced-sl-disc-1-2022"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="shop-thin-media2"></div>
+              <div className="shop-thin-text">
+                <h3>TCR ADVANCED SL DISC 1</h3>
+                <h5>$9,100</h5>
+                <p>
+                  THE LATEST GENERATION OF OUR FLAGSHIP ROAD RACING SERIES HAS
+                  BEEN WINNING THE BIGGEST RACES IN THE WORLD FOR MORE THAN
+                  THREE DECADES. IT’S THE ULTIMATE ALL-ROUNDER FOR THE
+                  PERFORMANCE OBSESSED.
+                </p>
+              </div>
+            </a>
+          </div>
+
+          <div className="shop-thin-main">
+            <a
+              href="https://www.trekbikes.com/us/en_US/bikes/road-bikes/performance-road-bikes/domane/domane-lt/domane-lt-9/p/35761/?colorCode=black"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="shop-thin-media3"></div>
+              <div className="shop-thin-text">
+                <h3>Domane+ LT 9</h3>
+                <h5>$12,549</h5>
+                <p>
+                  Domane+ LT 9 is a high-end carbon drop-bar electric bike
+                  designed for roadies who want an authentic road bike
+                  experience with an extra boost. The sleek and efficient Fazua
+                  drive system kicks things up a notch when you need it—like
+                  riding into a headwind or beating your buddy up a climb—but
+                  doesn't drag or hold you back when it's not in use. You also
+                  get the high-performance benefits of Shimano's best wireless
+                  electronic drivetrain, all-new Dura-Ace Di2.
+                </p>
+              </div>
+            </a>
+          </div>
+
+          <div className="shop-thin-main">
+            <a
+              href="https://www.cannondale.com/en-us/bikes/electric/e-mountain/moterra-neo-lt/moterra-neo-carbon-lt-2-c25572u"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="shop-thin-media4"></div>
+              <div className="shop-thin-text">
+                <h3>Carbon LT 2</h3>
+                <h5>$7,300</h5>
+                <p>
+                  Breaking Boundaries of how an E-MTN bike handles, Moterra Neo
+                  LT is in a league of its own. It’s a big, long travel
+                  e-mountain bike that rides like a snappy trail bike - agile
+                  and quick on the climbs, stable and composed on even the
+                  nastiest descents. It’s the kind of machine that makes you a
+                  better rider, wherever you ride.
+                </p>
+              </div>
+            </a>
+          </div>
+
+          <div className="shop-thin-main">
+            <a
+              href="https://www.cannondale.com/en-us/bikes/electric/e-mountain/moterra-neo-lt/moterra-neo-carbon-lt-2-c25572u"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="shop-thin-media5"></div>
+              <div className="shop-thin-text">
+                <h3>SUPER73-RX MOJAVE</h3>
+                <h5>$3,695</h5>
+                <p>
+                  The SUPER73-RX Mojave showcases all of SUPER73’s iconic
+                  features; the powerful R-series drive system, along with
+                  upgraded brakes and fully adjustable dual suspension, making
+                  the RX our top-of-the-line performance model. Fitted with our
+                  GRZLY All -Terrain Tires, the RX Mojave is comparable to none
+                  when it comes to off-road trail riding.
+                </p>
+              </div>
+            </a>
+          </div>
+
+          <div className="shop-thin-main">
+            <a
+              href="https://www.cannondale.com/en-us/bikes/electric/e-mountain/moterra-neo-lt/moterra-neo-carbon-lt-2-c25572u"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="shop-thin-media6"></div>
+              <div className="shop-thin-text">
+                <h3>Colnago Master</h3>
+                <h5>$$$$ unknown</h5>
+                <p>
+                  The SUPER73-RX Mojave showcases all of SUPER73’s iconic
+                  features; the powerful R-series drive system, along with
+                  upgraded brakes and fully adjustable dual suspension, making
+                  the RX our top-of-the-line performance model. Fitted with our
+                  GRZLY All -Terrain Tires, the RX Mojave is comparable to none
+                  when it comes to off-road trail riding.
+                </p>
+              </div>
+            </a>
+          </div>
+
+          <div className="shop-thin-main">
+            <a
+              href="https://www.cannondale.com/en-us/bikes/electric/e-mountain/moterra-neo-lt/moterra-neo-carbon-lt-2-c25572u"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="shop-thin-media7"></div>
+              <div className="shop-thin-text">
+                <h3>State Bicycle - Steel line</h3>
+                <h5>$549</h5>
+                <p>
+                  The 4130 Steel Line earned its namesake from the double-butted
+                  4130-grade chromoly steel used on the frame and fork that
+                  maximizes strength without making compromises on weight. Now
+                  with completely redesigned geometry to accommodate larger tire
+                  clearance and equipped with fork and seat stay rack mounts to
+                  be the perfect all-rounder.
+                </p>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </>
