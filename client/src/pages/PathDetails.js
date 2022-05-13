2324ccef90f5256cf6b41723cc912d75cf948168
Yusong Shi <jackshi.sys@gmail.com>
2022-05-13 11:26:42 -0400

    detailed page done

--- client/src/pages/PathDetails.js
+++ client/src/pages/PathDetails.js
@@ -6,6 +6,10 @@ import { useParams } from "react-router-dom";
 import Nav1 from "../components/Nav1";
 import Footer1 from "../components/Footer1";
 import "../style/pathdetails.css";
+import photo from "../assest/Grogu.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function PathDetails() {
   const [path, setPath] = useState("");
@@ -28,14 +32,23 @@ function PathDetails() {
         <Nav1 />
         <div className="pathdetail-container">
           <div className="pathdetail-left">
+            <Link to="/explore">
+              <div className="backarrow">
+                <FontAwesomeIcon icon={faArrowLeftLong} size="lg" />
+                &nbsp;&nbsp;<span>Go Back</span>
+              </div>
+            </Link>
             <h1>{path.name}</h1>
-            <img src={path.photo_url} alt=""></img>
-            <div>
-              Reviews:
+            <img className="pathdetails-main" src={path.photo_url} alt=""></img>
+            <div className="pathdetail-review">
+              <h3>Reviews:</h3>
               {path.reviews.map((review, index) => {
                 return (
-                  <div key={index}>
-                    <h4>{review.name}</h4>
+                  <div className="detail-review" key={index}>
+                    <div className="detail-user-info">
+                      <img src={photo}></img>
+                      <h4>{review.name}</h4>
+                    </div>
                     <p>-{review.description}</p>
                   </div>
                 );
@@ -43,11 +56,22 @@ function PathDetails() {
             </div>
           </div>
           <div className="pathdetail-right">
-            <p>Description{path.description}</p>
-            <p>Start: {path.start_point}</p>
-            <p>End: {path.end_point}</p>
-            <p>Path Type: {path.route_type}</p>
-            <p>Elevation: {path.elevation}</p>
+            <h5>{path.description}</h5>
+            <p>
+              Start: &nbsp;&nbsp;<strong>{path.start_point}</strong>
+            </p>
+            <p>
+              End: &nbsp;&nbsp;<strong>{path.end_point}</strong>
+            </p>
+            <p>
+              Path Type: &nbsp;&nbsp;<strong>{path.route_type}</strong>
+            </p>
+            <p>
+              Length: &nbsp;&nbsp;<strong>{path.length}</strong>
+            </p>
+            <p>
+              Elevation: &nbsp;&nbsp;<strong>{path.elevation}</strong>
+            </p>
           </div>
         </div>
         <Footer1 />
